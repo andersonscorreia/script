a5d2c3a3202a93b8bb0afb9ed240c8d0746008d9
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2022-08-30 20:13:08 -0300

    Update novo_usuario.sh

--- cadastroUser/novo_usuario.sh
+++ cadastroUser/novo_usuario.sh
@@ -5,7 +5,7 @@ if [ `cut -d ':' -f1 /etc/passwd | grep -w $USUARIO`  ]; then
 	echo 'Usuario ja cadastrado'
 else
 	if [ `cut -d ':' -f1 /etc/group | grep -w $GRUPO` ];then
-		adduser --gecos GECOS --home /home/$GRUPO/$USUARIO $USUARIO
+		adduser --disabled-password --gecos GECOS --home /home/$GRUPO/$USUARIO $USUARIO
 		passwd -e $USUARIO
 		adduser $USUARIO $GRUPO 
 		cp regras.txt /home/$GRUPO/$USUARIO
