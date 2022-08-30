21ade2814e1ff506a831c0a0986478669f492691
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2022-08-30 02:35:00 -0300

    Script de cadastro de usuario

---
+++ cadastroUser/novo_usuario.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+USUARIO=$2
+GRUPO=$1
+if [ `cut -d ':' -f1 /etc/passwd | grep -w $USUARIO`  ]; then
+	echo 'Usuario ja cadastrado'
+else
+	if [ `cut -d ':' -f1 /etc/group | grep -w $GRUPO` ];then
+		adduser --home /home/$GRUPO/$USUARIO $USUARIO
+		passwd -e $USUARIO
+		adduser $USUARIO $GRUPO 
+		cp /home/anderson/Documentos/regras.txt /home/$GRUPO/$USUARIO
+		echo -e " Olá $USUARIO, seja bem-vindo(a) à EMPRESA X. \n Seu cargo na instituição é de: $GRUPO \n A ativação do seu cadastro foi realizada em: `date +"%d de %h de %Y às %H:%M:%S"` \n Bom trabalho!" >> /home/$GRUPO/$USUARIO/welcome_$USUARIO.txt				
+		
+		
+				
+		echo 'usuario criado'		
+	else
+		echo 'Grupo não Existente'
+	fi     
+fi
+
