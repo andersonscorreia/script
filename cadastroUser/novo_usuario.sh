da21c4764489cec218d8082f9104ff10b8b29057
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2022-09-02 17:18:13 -0300

    Update novo_usuario.sh

--- cadastroUser/novo_usuario.sh
+++ cadastroUser/novo_usuario.sh
@@ -5,8 +5,7 @@ LEN=$#
 
 
 
-cadastro() {
-	if [ `cut -d ':' -f1 /etc/group | grep -w $GRUPO` ];then
+cadastro(USUARIO,GRUPO) {	
 		adduser --disabled-password --gecos GECOS --home /home/$GRUPO/$USUARIO $USUARIO
 		passwd -d $USUARIO
 		passwd -e $USUARIO
@@ -14,9 +13,7 @@ cadastro() {
 		cp regras.txt /home/$GRUPO/$USUARIO
 		echo -e " Olá $USUARIO, seja bem-vindo(a) à EMPRESA X. \n Seu cargo na instituição é de: $GRUPO \n A ativação do seu cadastro foi realizada em: `date +"%d de %h de %Y às %H:%M:%S"` \n Bom trabalho!" >> /home/$GRUPO/$USUARIO/welcome_$USUARIO.txt	
 		echo 'usuario criado'		
-	else
-		echo 'Grupo não Existente'
-	fi 
+	
 
 }
 
@@ -25,15 +22,15 @@ if [ $LEN -eq 2 ]; then
 	if [ `cut -d ':' -f1 /etc/passwd | grep -w $USUARIO`  ]; then
 		echo 'Usuario ja cadastrado'
 	else
-		if [ $TIPO = 'professor']; then
+		if [ $TIPO -eq 'professor']; then
 			GRUPO = 'professores'
-			cadastro()
-		elif [ $TIPO = 'aluno' ]; then
+			cadastro($USUARIO,$GRUPO)
+		elif [ $TIPO -eq 'aluno' ]; then
 			GRUPO = 'alunos'
-			cadastro()
-		elif [ $TIPO = 'tecnico' ]; then
+			cadastro($USUARIO,$GRUPO)
+		elif [ $TIPO -eq 'tecnico' ]; then
 			GRUPO = 'tecnicos'
-			cadastro()
+			cadastro($USUARIO,$GRUPO)
 		else:
 			echo 'Tipo de usuario não pode ser cadastrado'
 			
